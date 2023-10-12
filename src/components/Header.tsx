a5e482e0f882166ba80d6d4bd8a9d5d1f2c0d8df
João Victor <jvicdev@proton.me>
2023-10-12 20:48:46 -0300

    Move line-height class to body tag.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,9 @@ function Header() {
   return (
     <header className="container mx-auto flex h-20 items-center border-b px-6 sm:px-0">
       <nav className="flex w-full items-center justify-between">
-        <h1 className="text-2xl leading-normal">JVSP.</h1>
+        <h1 className="text-2xl">JVSP.</h1>
         <div className="flex items-center gap-4">
-          <ul className="hidden items-center gap-8 text-lg leading-normal md:flex">
+          <ul className="hidden items-center gap-8 text-lg md:flex">
             {links.map((link) => (
               <li key={link.path}>
                 <a href={link.path}>{link.label}</a>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,23 +2,20 @@ import { ArrowRight } from "@phosphor-icons/react";
 
 function Hero() {
   return (
-    <div className="container mx-auto flex min-h-[calc(100vh_-_7.5rem)] flex-col justify-center gap-16 px-6 sm:px-0">
+    <div className="container mx-auto flex min-h-[calc(100dvh_-_5rem)] flex-col justify-center gap-16 px-6 sm:px-0">
       <main className="flex flex-col gap-2">
         <span>Olá, me chamo</span>
-        <h1 className="bg-gradient-to-r from-fuchsia-500 via-cyan-500 to-pink-500 bg-clip-text text-5xl font-bold leading-normal text-transparent lg:text-8xl">
+        <h1 className="bg-gradient-to-r from-fuchsia-500 via-cyan-500 to-pink-500 bg-clip-text text-5xl font-bold text-transparent lg:text-8xl">
           João Victor
         </h1>
-        <p className="leading-normal md:text-lg">
+        <p className="md:text-lg">
           Sou um <b>Desenvolvedor Front-end</b>, com paixão em criar interfaces
           de usuário eficientes e visualmente atraentes. Sou comprometido em
           aprimorar minhas habilidades constantemente, buscando alcançar
           excelência em cada projeto.
         </p>
       </main>
-      <a
-        href="#contact"
-        className="flex items-center gap-3 text-xl font-light leading-normal"
-      >
+      <a href="#contact" className="flex items-center gap-3 text-xl font-light">
         Entrar em contato
         <ArrowRight size={24} />
       </a>
