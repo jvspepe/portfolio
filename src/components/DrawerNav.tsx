eeae4ce50d7f8e1685d5afd3e3dd9f07904c78a3
João Victor <jvicdev@proton.me>
2023-10-12 19:55:56 -0300

    Add navigation drawer.

---
+++ src/components/DrawerNav.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { List, X } from "@phosphor-icons/react";
+import { Link } from "../@types/link";
+import Drawer from "./Drawer";
+import IconButton from "./IconButton";
+
+type Props = {
+  links: Link[];
+};
+
+function DrawerNav({ links }: Props) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    document.documentElement.style.overflowY = isOpen ? "hidden" : "visible";
+    return () => {
+      document.documentElement.style.overflowY = "visible";
+    };
+  }, [isOpen]);
+
+  return (
+    <div className="md:hidden">
+      <Drawer
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        position="right"
+        handler={
+          <IconButton
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Abrir menu"
+            aria-haspopup="true"
+            aria-controls="nav-list"
+            aria-expanded={isOpen}
+            title="Abrir menu"
+            type="button"
+          >
+            <List size={24} />
+          </IconButton>
+        }
+      >
+        <div className="flex flex-col">
+          <div className="flex h-20 w-full items-center justify-end pr-6">
+            <IconButton
+              onClick={() => setIsOpen(!isOpen)}
+              aria-label="Fechar Menu"
+              aria-controls="nav-list"
+              aria-expanded={isOpen}
+              title="Fechar menu"
+              type="button"
+            >
+              <X size={24} />
+            </IconButton>
+          </div>
+          <ul
+            id="nav-list"
+            aria-hidden={!isOpen}
+            className="bg-neutral-50 text-neutral-950"
+          >
+            {links.map((link) => (
+              <li key={link.path} className="flex w-full">
+                <a
+                  href={link.path}
+                  className="w-full border-t border-neutral-200 p-4 transition-colors duration-200 hover:bg-neutral-200"
+                >
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+
+export default DrawerNav;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,31 @@
 import { Sun } from "@phosphor-icons/react";
+import { Link } from "../@types/link";
+import DrawerNav from "./DrawerNav";
+import IconButton from "./IconButton";
+
+const links: Link[] = [
+  { label: "Projetos", path: "#projetos" },
+  { label: "Currículo", path: "curriculum" },
+  { label: "Contato", path: "#contato" },
+];
 
 function Header() {
   return (
-    <header className="container mx-auto flex h-[120px] items-center">
+    <header className="container mx-auto flex h-20 items-center border-b px-6 sm:px-0">
       <nav className="flex w-full items-center justify-between">
         <h1 className="text-2xl leading-normal">JVSP.</h1>
-        <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8 text-lg leading-normal">
-            <li>
-              <a href="#projects">Projetos</a>
-            </li>
-            <li>
-              <a href="#curriculum">Currículo</a>
-            </li>
-            <li>
-              <a href="#contact">Contato</a>
-            </li>
+        <div className="flex items-center gap-4">
+          <ul className="hidden items-center gap-8 text-lg leading-normal md:flex">
+            {links.map((link) => (
+              <li key={link.path}>
+                <a href={link.path}>{link.label}</a>
+              </li>
+            ))}
           </ul>
-          <button aria-label="Mudar tema" title="Mudar tema">
+          <IconButton aria-label="Mudar tema" title="Mudar tema">
             <Sun size={24} weight="fill" />
-          </button>
+          </IconButton>
+          <DrawerNav links={links} />
         </div>
       </nav>
     </header>
